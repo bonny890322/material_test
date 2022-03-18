75259d61aac620906b55b3787cf5cf63e16c7494
dai <71811787+bonny890322@users.noreply.github.com>
2022-03-18 18:44:05 +0800

    feat: 新增導覽列、form表單

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,72 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Component
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { FormComponent } from './form/form.component';
+
+//material
+import { MatSliderModule } from '@angular/material/slider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    MatMenuModule,
+    MatCardModule,
+    MatButtonModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatTableModule,
+    MatListModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatStepperModule,
+    MatDialogModule,
+    MatRadioModule,
+    MatTabsModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: "login", component: FormComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
